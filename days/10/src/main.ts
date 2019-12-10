d18eca9232626a1bcfa6419544024889f197fb2f
Georgios Kaleadis <georgios@kaleadis.de>
2019-12-10 22:59:28 +0100

    found a solution for part 1

--- days/10/src/main.ts
+++ days/10/src/main.ts
@@ -9,7 +9,7 @@ import line from 'bresenham-line';
 async function part01() {
     console.log('part01');
 
-    const input = await fetch('./example01.txt');
+    const input = await fetch('./input.txt');
     const data = await input.text();
     const coordData = data.split('');
     const {asteroids} = coordData.reduce((accu, current) => {
@@ -31,9 +31,35 @@ async function part01() {
         return  accu;
     }, {asteroids:[] as any, pointer: {x:0, y:0}});
 
-    const result = line({x:3, y:4}, {x:4, y:0});
-    const result2 = line({x:3, y:4}, {x:1, y:0});
-    console.log({r1: Array.from(result), r2: Array.from(result2)});
+    const visibleValues = asteroids.map(point => calculateVisible(asteroids, point));
+    const slopes = withSlopes(asteroids, {x:2, y:2});
+    console.log({visibleValues, slopes});
+
+    const maxValue = Math.max(...visibleValues);
+    console.log({maxValue});
+}
+
+function calculateVisible(allAsteroids, point) {
+    const slopes = withSlopes(allAsteroids, point);
+    const values = slopes.map((item) => item.slope);
+    const uniqFilter = (value, index, self) => self.indexOf(value) === index;
+    const valuesUniq = values.filter( uniqFilter );
+    // return { [`${point.x}-${point.y}`]: valuesUniq.length};
+    return valuesUniq.length};
+}
+
+function withSlopes(points, point) {
+    return points.map(({x, y}) => {
+        const x2 = x - point.x;
+        const y2 = y - point.y;
+        return {
+            x: x2,
+            y: y2,
+            slope: Math.atan2(y2,x2),
+            original: {x, y},
+            testPoint: {...point}
+        }
+    }).filter(value => !(value.x == 0 && value.y == 0))
 }
 
 
