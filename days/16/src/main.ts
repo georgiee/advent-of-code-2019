39861c0ba65d1d862eb05c96bbaaa07f97cb7789
Georgios Kaleadis <georgios@kaleadis.de>
2019-12-16 08:12:41 +0100

    feat:  day 16, part 2 failed, bruteforce is not working underlying pattern not discovered

--- days/16/src/main.ts
+++ days/16/src/main.ts
@@ -8,19 +8,52 @@ async function run() {
     const input = await fetch('./input.txt');
     const data = await input.text();
 
-    const digits = data.split('')
+    const digits = '12345678'.split('')
             .map(value => parseInt(value, 10))
             .filter(value => Number.isInteger(value));
 
-
-    part1(digits);
-    // part2(data);
+console.log(digits)
+    // part1(digits);
+    part2(digits);
 }
+
+function repeat(digits, times) {
+    let counter =  times;
+    let result = [];
+    while(counter-- > 0) {
+        result = result.concat(digits);
+    }
+    return result;
+}
+
+function part2(digits) {
+    //  that's most likely WRONG, as each
+    const fullDigitSet = repeat(digits, 2);
+    const offset = parseInt(digits.slice(0, 8).join(''),10);
+    console.log('run1'  );
+    const basephase =  [0, 1, 0, -1];
+    const phaseMaker = depth => makePhase(basephase, depth);
+    const fftMaker = digitList => makeFFT(digitList, phaseMaker);
+    const fft = fftMaker(digits);
+    const result = (fft.next().value);
+
+    console.log('run2');
+    const fft2 = fftMaker(fullDigitSet);
+    const result2 = (fft2.next().value);
+    // nothing that points to the underlying systematic
+    // instead of brute forcing through the full  set of
+    // a  fullDigitSet with 1000 times repeated digits.
+    console.log(result, result2);
+}
+
+function getResult(digits, offset = 0) {
+    return digits.slice(offset, 8);
+}
+
 function part1(digits){
     const basephase =  [0, 1, 0, -1];
     const phaseMaker = depth => makePhase(basephase, depth);
     const fftMaker = digitList => makeFFT(digitList, phaseMaker);
-
     const fft = fftMaker(digits);
 
     let rounds = 100;
@@ -34,12 +67,7 @@ function part1(digits){
     console.log('part 1', current.join(''));
 }
 
-function part2() {
-
-}
-
 function* makeFFT(digits, phaseMaker) {
-    console.log('applyPhases', digits);
     let currentDigits = digits;
 
     while(true){
@@ -50,8 +78,7 @@ function* makeFFT(digits, phaseMaker) {
                 const phaseValue = phase.next().value;
                 return result  + phaseValue * digit
             }, 0);
-
-            let  result =  Math.abs(sum)%10;
+            let result =  Math.abs(sum)%10;
             return result;
         });
         currentDigits = newDigits;
